a34f5e1f8a07ecca5b97fb9c645565550d68ccb2
Dev Lanageuse <dev@lanageuse.com>
2025-02-11 10:54:26 +0100

    add new app class

---
+++ scripts/App.js
@@ -0,0 +1,32 @@
+import Api from './api/Api.js' 
+import Photograph from './models/photograph.js'
+import CardProfil from './views/CardProfil.js'
+
+class App{
+    constructor(){
+        this.photographers = []
+        this.dataApi = new Api('./data/photographers.json', 'photographers')
+        this.$wrapper = document.getElementById('main')
+        this.$dataWrapper = document.querySelector('.photographer_section')
+    }
+
+    async fetchData(){
+        const photographers  = await this.dataApi.get()
+        this.photographers = photographers.map(photograph => new Photograph(photograph))
+        return this.photographers
+    }
+
+    async displayProfil(){
+        this.photographers.map(data =>{
+            const Template = new CardProfil(data)
+            this.$dataWrapper.appendChild(Template.createCard())
+        })
+    }
+    async main(){
+        await this.fetchData()
+        await this.displayProfil()   
+    }
+}
+
+const app = new App()
+app.main()
