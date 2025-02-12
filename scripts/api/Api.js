ce070461b3251e61d6989964f95b70d8d5c1f7d5
Dev Lanageuse <dev@lanageuse.com>
2025-02-12 17:15:02 +0100

    Refactored APi class : removed property extraction, now returning full data object instead of a single node

--- scripts/api/Api.js
+++ scripts/api/Api.js
@@ -1,15 +1,13 @@
 class Api{
-    constructor(url, property){
+    constructor(url){
         this._url = url
-        this._property = property
     }
-
     async get(){
         try {
             const response = await fetch(this._url)
             if(!(response.ok)) throw new Error(`Error http : ${response.status}`)
             const data = await response.json()
-            return data[this._property]
+            return data
         } catch (error) {
             throw new Error('Format de données inconnu', {error : error})
         }
