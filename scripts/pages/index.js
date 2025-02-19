994b54054d45c33f1a31f4420f00118360e6cf9a
Dev Lanageuse <dev@lanageuse.com>
2025-02-19 10:18:39 +0100

    Added the logic for localStorage data.

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,5 +1,5 @@
 import Api from '../api/Api.js' 
-import Photograph from '../models/photograph.js'
+import Photograph from '../models/Photograph.js'
 import CardProfile from '../templates/CardProfile.js'
 
 class Index{
@@ -8,12 +8,23 @@ class Index{
         this.dataApi = new Api('./data/photographers.json')
         this.$wrapper = document.getElementById('main')
         this.$dataWrapper = document.querySelector('.photographer_section')
+        this.dataLocal= localStorage.getItem("photographers") || null
     }
 
     async fetchData(){
-        const {photographers, media}  = await this.dataApi.get()
-        this.photographers = photographers.map(photograph => new Photograph(photograph))
+        if(this.dataLocal){
+            const photographers = JSON.parse(this.dataLocal)
+            this.photographers = photographers.map(photograph => new Photograph(photograph))
+        }else{
+            const {photographers, media}  = await this.dataApi.get()
+            this.photographers = photographers.map(photograph => new Photograph(photograph))
+            this.saveData(this.photographers, "photographers")
+        }
         return this.photographers
+    }
+
+    saveData(data, item){
+        localStorage.setItem(item, JSON.stringify(data))
     }
 
     async displayProfiles(){
