3cdb7c603c85917367dc1a59f8add7f317fb1534
Dev Lanageuse <dev@lanageuse.com>
2025-02-19 10:21:35 +0100

    Added local storage logic.

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,11 +1,11 @@
 import Api from '../api/Api.js'
-import Photograph from '../models/photograph.js'
+import Photograph from '../models/Photograph.js'
 import HeaderProfile from '../templates/HeaderProfile.js'
 import Gallery from '../templates/Gallery.js'
 import { handleLikes } from '../utils/likes.js'
 import openCloseModal from '../utils/modal.js'
-import {contactForm} from '../utils/contactForm.js'
-import {handleFilter} from '../utils/filter.js'
+import { contactForm } from '../utils/contactForm.js'
+import { handleFilter } from '../utils/filter.js'
 import { displayLightbox } from '../utils/lightbox.js'
 
 const params = new URLSearchParams(document.location.search);
@@ -19,47 +19,59 @@ class PhotographerPage {
         this.$wrapperProfile = document.querySelector('.photograph-header')
         this.$wrapperGallery = document.querySelector('.gallery-container')
         this.$wrapperWidget = document.querySelector('.widget')
+        this.dataLocal = localStorage.getItem(`${this.id}`) || null
     }
     async getProfile() {
-        if(typeof(this.id) !== 'number'){
+        if (typeof (this.id) !== 'number') {
             throw new Error('ID must have a number')
         }
-        const {photographers, media} = await this.api.get()
+        const { photographers } = await this.api.get()
         const data = photographers.filter(profile => profile.id === this.id)
         this.profile = new Photograph(data[0])
         return this.profile
     }
-    async displayProfile(){
+    async displayProfile() {
         const Template = new HeaderProfile(this.profile)
         this.$wrapperProfile.innerHTML = Template.createCard()
-    }    
+    }
     async getGallery() {
-        const {photographers, media}= await this.api.get()
-        this.gallery = media.filter(media => media.photographerId === this.id)
+        if (this.dataLocal) {
+            const media = JSON.parse(this.dataLocal)
+            this.gallery = media.filter(media => media.photographerId === this.id)
+        } else {
+            const { media } = await this.api.get()
+            this.gallery = media.filter(media => media.photographerId === this.id)
+            this.saveGallery(this.id, this.gallery)
+        }
         return this.gallery
     }
-     async displayGallery(sortedGallery = null){
+
+    saveGallery(item, data){
+        localStorage.setItem(item, JSON.stringify(data))
+    }
+
+    async displayGallery(sortedGallery = null) {
         const photograph = this.profile
         const medias = sortedGallery || this.gallery
         const Template = new Gallery(medias, photograph)
         this.$wrapperGallery.innerHTML = Template.createGallery()
         this.$wrapperWidget.innerHTML = Template.createWidget()
-     }
-    
+    }
+
     async main() {
         await this.getProfile()
         await this.displayProfile()
-        
+
         await this.getGallery()
-        await this.displayGallery().then( setTimeout(() => this.$wrapperWidget.classList.add('show'), 1000) )
+        await this.displayGallery().then(setTimeout(() => this.$wrapperWidget.classList.add('show'), 1000))
 
         await handleLikes()
-        
-        openCloseModal()
-        contactForm();
 
-        handleFilter(this.gallery, (sortedGallery) => this.displayGallery(sortedGallery))
-        
+        openCloseModal()
+        contactForm(this.profile);
+
+        handleFilter(this.gallery, (sortedGallery) => this.displayGallery(sortedGallery), this.profile)
+
         displayLightbox(this.gallery, this.profile)
     }
 }
