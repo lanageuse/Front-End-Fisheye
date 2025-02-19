611d1163c226d18e8da757c5093d8fe1ee06f4c4
Dev Lanageuse <dev@lanageuse.com>
2025-02-19 10:17:36 +0100

    Added the new method toJSON for formated data for export to the localSTorage

--- scripts/models/Photograph.js
+++ scripts/models/Photograph.js
@@ -1,5 +1,5 @@
 class Photograph {
-    constructor({name, id, city, country, tagline, price, portrait}) {
+    constructor({ name, id, city, country, tagline, price, portrait }) {
         this._name = name || 'undefined'
         this._id = id || 'undefined'
         this._city = city || 'undefined'
@@ -12,11 +12,24 @@ class Photograph {
     get name() { return this._name }
     get id() { return this._id }
     get id() { return this._id }
-    get city(){ return this._city}
-    get country(){ return this._country}
-    get tagline(){ return this._tagline}
-    get price(){ return this._price}
-    get portrait(){ return this._portrait}
+    get city() { return this._city }
+    get country() { return this._country }
+    get tagline() { return this._tagline }
+    get price() { return this._price }
+    get portrait() { return this._portrait }
+
+    toJSON() {
+       return {
+        "name" : this._name,
+        "id" : this._id,
+        "id" : this._id,
+        "city" : this._city,
+        "country" : this._country,
+        "tagline" : this._tagline,
+        "price" : this._price,
+        "portrait" : this._portrait
+       }
+    }
 }
 
 export default Photograph
