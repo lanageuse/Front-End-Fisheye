2d5158cdcf9d5af62298296a4fcfaa982606dd27
Dev Lanageuse <dev@lanageuse.com>
2025-02-19 10:24:12 +0100

    Added the event observer for like

--- scripts/utils/likes.js
+++ scripts/utils/likes.js
@@ -1,25 +1,41 @@
 import EventObserver from "../likes/EventObserver.js"
 import Counter from "../likes/Counter.js"
+import AddToStorage from '../likes/AddToStorage.js'
 
+const params = new URLSearchParams(document.location.search);
+const photographId = params.get("id");
 
 const EventLikes = new EventObserver()
 const CounterLikes = new Counter()
+
+const EventStorage = new EventObserver()
+const LikeStorage = new AddToStorage()
+
 EventLikes.subscribe(CounterLikes)
+EventStorage.subscribe(LikeStorage)
 
 export const handleLikes = async () => {
     const btnLikes = document.querySelectorAll('.handleLikes')
+
     btnLikes.forEach((btn) => {
         btn.addEventListener("click", () => {
-            let likeCount = 0
-            btn.classList.toggle('liked')
+
             const likeWrapper = btn.querySelector('.count')
+            const media_id = btn.dataset.id
+            const photograph_id = photographId
+            let likeCount = 0
+
+            btn.classList.toggle('liked')
             likeCount = Number( likeWrapper.textContent)
+
             if(btn.classList.contains('liked')){
                 likeCount = likeCount + 1
                 EventLikes.notify("INC")
+                EventStorage.notify("ADD", [media_id, photograph_id] )
             }else{
                 likeCount = likeCount - 1
                 EventLikes.notify("DEC")
+                EventStorage.notify("DEL", [media_id, photograph_id] )
             }
             likeWrapper.textContent = likeCount
         })
