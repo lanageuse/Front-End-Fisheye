d785a81da22312a25d5b5642ef33d9448c79be19
Dev Lanageuse <dev@lanageuse.com>
2025-02-25 09:36:30 +0100

    Refactored like handling for better readability and keyboard accessibility.

--- scripts/utils/likes.js
+++ scripts/utils/likes.js
@@ -16,28 +16,35 @@ EventStorage.subscribe(LikeStorage)
 
 export const handleLikes = async () => {
     const btnLikes = document.querySelectorAll('.handleLikes')
+    const handleLike = (btn) => {
+        const likeWrapper = btn.querySelector('.count')
+        const media_id = btn.dataset.id
+        const photograph_id = photographId
+        let likeCount = 0
 
+        btn.classList.toggle('liked')
+        likeCount = Number(likeWrapper.textContent)
+
+        if (btn.classList.contains('liked')) {
+            likeCount = likeCount + 1
+            EventLikes.notify("INC")
+            EventStorage.notify("ADD", [media_id, photograph_id])
+        } else {
+            likeCount = likeCount - 1
+            EventLikes.notify("DEC")
+            EventStorage.notify("DEL", [media_id, photograph_id])
+        }
+        likeWrapper.textContent = likeCount
+    }
     btnLikes.forEach((btn) => {
         btn.addEventListener("click", () => {
-
-            const likeWrapper = btn.querySelector('.count')
-            const media_id = btn.dataset.id
-            const photograph_id = photographId
-            let likeCount = 0
-
-            btn.classList.toggle('liked')
-            likeCount = Number( likeWrapper.textContent)
-
-            if(btn.classList.contains('liked')){
-                likeCount = likeCount + 1
-                EventLikes.notify("INC")
-                EventStorage.notify("ADD", [media_id, photograph_id] )
-            }else{
-                likeCount = likeCount - 1
-                EventLikes.notify("DEC")
-                EventStorage.notify("DEL", [media_id, photograph_id] )
+            handleLike(btn)
+        })
+        btn.addEventListener("keydown", (e) => {
+            if (e.key === 'Enter') {
+                handleLike(btn)
             }
-            likeWrapper.textContent = likeCount
         })
     })
+
 }
